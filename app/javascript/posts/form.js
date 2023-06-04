b6329b8127a6b2b28ca0be6248e94b876681426b
yuuuba <nsabu45@gmail.com>
2023-06-04 15:43:54 +0900

    console表示まで

--- app/javascript/posts/form.js
+++ app/javascript/posts/form.js
@@ -6,9 +6,12 @@
   
   // 3. 以降は オブジェクトの key を参照することで、 value を読み出せる
 
-  
+
   // 例: JS から Rails にリクエストを送ると、
 
   // JSON {message: "Hello"} が返ってくる... JS 側で console.log() に結果を出力する、単機能アプリ
-  
-
+    fetch('/data.json')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
