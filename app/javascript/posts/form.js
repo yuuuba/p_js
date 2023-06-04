7d230589fccc67d75c50a42565990e596a214b0e
yuuuba <nsabu45@gmail.com>
2023-06-04 15:05:59 +0900

    first

---
+++ app/javascript/posts/form.js
@@ -0,0 +1,14 @@
+  // TODO:
+
+  // 1. Rails から JSON (String) を渡してもらう (JS 側からリクエストを送る.. fetch メソッド)
+
+  // 2. JS では JSON.parse() で オブジェクトに変換
+  
+  // 3. 以降は オブジェクトの key を参照することで、 value を読み出せる
+
+  
+  // 例: JS から Rails にリクエストを送ると、
+
+  // JSON {message: "Hello"} が返ってくる... JS 側で console.log() に結果を出力する、単機能アプリ
+  
+
